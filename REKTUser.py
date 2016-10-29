20307ba8e5d3606596df4972a760138e7e232713
Stephen Longofono <onofognol@ku.edu>
2016-10-29 14:46:26 -0500

    Update REKTUser with test cases

--- REKTUser.py
+++ REKTUser.py
@@ -1,9 +1,13 @@
 import Mutators
 import traceback
+import Variance
+import functools
 
 class User():
     def __init__(self, logfile="userProfile.txt", debug=False):
         self.logfile = logfile
+	self.debug = debug
+	self.profile = None
         self.labels = ['artists', 'genres', 'popularity', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'key', 'liveness', 'valence']
         self.processProfile(self.logfile)
         self.addVector =[
@@ -18,12 +22,37 @@ class User():
                             Mutators.livenessMutator,
                             Mutators.valenceMutator
                         ]
-        self.debug = debug
 
     # I'm cheap but volatile
     def addData(self, newDataVector):
         for i in range(len(self.addVector)):
             self.addVector[i](newVal=newDataVector[i], debug=self.debug)
+
+    def getSongDifferences(self, newSongVectors):
+	return  map(functools.partial(Variance.getVariance, self.profile),  newSongVectors)
+
+
+    def prettyPrintProfile(self):
+        for i in range(10):
+            print self.labels[i], ':'
+            print self.profile[i]
+
+
+    def processProfile(self, filename):
+        print 'Processing user profile data...'
+        self.profile = []
+        x = open(filename, 'r')
+        artists = x.readline()[2:-3].split(',,')
+        genres = x.readline()[2:-3].split(',,')
+        self.profile.append(artists)
+        self.profile.append(genres)
+        for line in x:
+            self.profile.append(float(line))
+
+        for i in self.profile:
+            print i
+        x.close()
+
 
     # I'm expensive but permenant
     def saveStatus(self):
@@ -50,30 +79,50 @@ class User():
             print 'Failed to save new profile...'
             traceback.print_exc()
 
-    def processProfile(self, filename):
-        print 'Processing user profile data...'
-        self.profile = []
-        x = open(filename, 'r')
-        artists = x.readline()[2:-3].split(',,')
-        genres = x.readline()[2:-3].split(',,')
-        self.profile.append(artists)
-        self.profile.append(genres)
-        for line in x:
-            self.profile.append(float(line))
-
-        for i in self.profile:
-            print i
-        x.close()
-
-    def prettyPrintProfile(self):
-        for i in range(10):
-            print self.labels[i], ':'
-            print self.profile[i]
 
 if __name__ == '__main__':
-    tester = User(debug=True)
-    tester.prettyPrintProfile()
-    tester.addData([['Katy MF Perry', 'Bob Marley, Bob Vila, and Bob Ross'], ['Death Pop', 'Icelandic glam-folk'], 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0])
-    tester.prettyPrintProfile()
-    tester.saveStatus()
-    tester.prettyPrintProfile()
+
+	# Basic setup
+	tester = User(debug=True)
+	tester.prettyPrintProfile()
+	tester.addData([['Katy MF Perry', 'Bob Marley, Bob Vila, and Bob Ross'], ['Death Pop', 'Icelandic glam-folk'], 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0])
+	tester.prettyPrintProfile()
+	tester.saveStatus()
+	tester.prettyPrintProfile()
+
+	# Comparison
+	# Build test cases
+	newSongs = []
+	artists = ['The Unicorns','New Artist 1','new Artist 2','of Montreal','Mr. Bungle','RJD2', 'TV On The Radio', 'The Beatles', 'The Who', 'The Rolling Stones']
+	genres = ['blues-rock','ok indie','power pop','psychedelic rock','slow core','space rock','alternative pop','jangle pop','new wave','post-hardcore','post-punk','punk','rock','shoegaze','uk post-punk','album rock','art rock','classic funk rock','classic rock','dance rock','folk rock','glam rock','hard rock','mellow gold','new wave pop','permanent wave','pop christmas','protopunk','singer-songwriter','soft rock','funk','soul','new weird america','pop rock','post-grunge','big beat','downtempo','folk-pop','ambeat','electro house','classic soundtrack','library music','spytrack','indie garage rock','indie poptimism','metropopolis','shimmer pop','compositional ambient','post rock','australian dance','electroclash','filter house','pop']
+	for i in range(10):
+		newSong = [
+			[artists[i]],
+			[genres[i]],
+			i,
+			i,
+			i,
+			i,
+			i,
+			i,
+			i,
+			i
+			]
+		newSongs.append(newSong)
+	for i in newSongs:
+		print i
+
+	diffs = tester.getSongDifferences(newSongs)
+	bestSong = None
+	bestVal = 200000
+	for i in range(len(newSongs)):
+		print "New Song artist: ", newSongs[i][0][0]
+		print "New Song genre: ", newSongs[i][1][0]
+		print "New Song total difference: ", sum(diffs[i])
+		if sum(diffs[i]) < bestVal:
+			bestVal = sum(diffs[i])
+			bestSong = newSongs[i][0][0]
+
+	print "Most similar new song is: "
+	print bestSong, " with a total difference ", bestVal
+
