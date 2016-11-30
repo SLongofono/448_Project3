26b51280ef0237d5edf280795fb435f730d0847f
Grant Jurgensen <g953j882.ku.edu>
2016-11-30 09:23:00 -0600

    added documentation to PlaylistGenerator, removed unused code

--- PlaylistGenerator.py
+++ PlaylistGenerator.py
@@ -2,7 +2,16 @@ import sys
 import spotipy
 import spotipy.util as util
 
-#creates or finds playlist with that name
+##  getPlaylist
+# @brief finds a playlist of a given name, or creates a new playlist if none are found
+# @param user the spotify username of the user
+# @param name the name of the playlist
+# @return A spotify playlist id
+# @details searches for a spotify playlist in the users account. Note, it can
+#         see public playlists. If it finds a playlist matching the name paramater
+#         it will return this playlists id. Otherwise, it will create a new playlist
+#         with the name it passed, and return this newly created playlist's id.
+#
 def getPlaylist(user, name):
         scope = 'playlist-modify-public'
         usageToken = util.prompt_for_user_token(user, scope)
@@ -14,9 +23,16 @@ def getPlaylist(user, name):
                                 return playlist['id']
                 #if no playlist has been found...
                 return sp.user_playlist_create(user, name, True)['id']
-            
-#playlist does not have to already exist - handled by getPlaylist function
-#songs = list of track ids
+
+##  addToPlaylist
+# @brief adds a list of songs to a spotify playlist
+# @param user the spotify username of the user
+# @param name the name of the playlist
+# @param songs a list of spotify track ids
+# @return none
+# @details gets a playlist matching the name parameter. If none already exist, a new 
+#         playlist will be created.  Then adds the list of songs to the playlist.
+#
 def addToPlaylist(user, name, songs): #add songs parameter
         scope = 'playlist-modify-public'
         usageToken = util.prompt_for_user_token(user, scope)
@@ -26,6 +42,16 @@ def addToPlaylist(user, name, songs): #add songs parameter
 		sp.user_playlist_add_tracks(user, playlist_id, songs)
 
 #removes all tracks but does not delete playlist (leaves an empty playlist)
+
+##  clearPlaylist
+# @brief removes all songs from a user playlist
+# @param user the spotify username of the user
+# @param playlist_id the id of the spotify playlist
+# @param songs a list of spotify track ids
+# @details removes every song from a spotify playlist, leaving it empty. Note, the 
+#         playlist must be public for it to be found. The resulting playlist is not, 
+#         removed, just empty.
+#
 def clearPlaylist(user, playlist_id):
         scope = 'playlist-modify-public'
         usageToken = util.prompt_for_user_token(user, scope)
@@ -37,17 +63,7 @@ def clearPlaylist(user, playlist_id):
                         track_ids.append(track['track']['id'])
                 sp.user_playlist_remove_all_occurrences_of_tracks(user, playlist_id, track_ids)
 
-#temporary test method
-def temp_test(user, query):
-        scope = 'playlist-modify-public'
-        usageToken = util.prompt_for_user_token(user, scope)
-        if usageToken:
-                sp = spotipy.Spotify(auth=usageToken)
-                results = sp.search(query, 5)
-                
-                for track in results['tracks']['items']:
-                        print track['id']
-
+#testing method
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		user = sys.argv[1]
