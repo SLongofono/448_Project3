51edf96a5311829dcf9f937a5fe4d5ddbfad324b
Grant Jurgensen <g953j882.ku.edu>
2016-12-03 17:39:27 -0600

    Made the tests more verbose, more print to console explaining what is being tested

--- testScripts/testAssembleProfile.py
+++ testScripts/testAssembleProfile.py
@@ -7,6 +7,7 @@ import Assemble_Profile
 user = config_obj.get_user()
 
 def testGetUserSongVectors():
+        print "Getting song vectors from user library"
         songVectors = Assemble_Profile.getUserSongVectors(user, numCalls=1, numEntries=1)
         return (len(songVectors) > 0)
 
@@ -25,15 +26,20 @@ def testGetVectorFromTrack():
                 search = sp.search('radiohead', limit=1,)
                 track = search['tracks']['items'][0]
                 
+                print "Building vector from track"
                 featureVector = Assemble_Profile.getVectorFromTrack(sp, sp.audio_features([track['id']])[0], track['artists'])
                 
                 expectedVector = [['Radiohead'], ['alternative rock', 'indie rock', 'melancholia', 'permanent wave', 'rock'], 400, 0.0102, 0.515, 0.43, 0.000141, 7, 0.129, 0.096]
                 
+                print "Comparing to expected values"
                 return (featureVector == expectedVector)
 
 def testAllAssembleProfile():
-        print "Testing get user songs method... " + if testGetUserSongVectors() else "Failed."
-        print "Testing track to vector method... " + "Passed." if testGetVectorFromTrack() else "Failed."
+        print "\nTesting getUserSongVectors() method... "
+        print "Passed." if testGetUserSongVectors() else "Failed."
+                
+        print "\nTesting getVectorFromTrack() method... "
+        print "Passed." if testGetVectorFromTrack() else "Failed."
 
 if __name__ == '__main__':
         testAllAssembleProfile()
