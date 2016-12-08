4b2f907af8930868e6b3d7edaad439117b8aa4d4
Grant Jurgensen <g953j882.ku.edu>
2016-12-08 16:51:48 -0600

    made tests more verbose, explains what is being tested

--- testREKTUser.py
+++ testREKTUser.py
@@ -8,8 +8,10 @@ def testAddData(ru):
         testSong = [['Sufjan Stevens', 'The National'], ['Indie folk', 'Indie Rock'], 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]
         expectedResult = [['Sufjan Stevens', 'The National'], ['Indie folk', 'Indie Rock'], [1.0], [2.0], [3.0], [4.0], [5.0], [6.0], [7.0], [8.0]]
         
+        print "Adding test song into REKTUser's mutators"
         ru.addData(testSong)
         
+        print "Searching for new song in REKTUser's addVector data"
         songFound = True
         for i in range(len(ru.addVector)):
             #print ru.addVector[i]()
@@ -17,16 +19,18 @@ def testAddData(ru):
             
             if ru.addVector[i]() != expectedResult[i]:
                 songFound = False
-                
+        
         return songFound
 
 def testStdDev(ru):
+        print "Calculating standard deviation"
         ru.calculateStandardDeviations()
         return (len(ru.stdDevs) == 8)
     
 def testSongDiffs(ru):
         testSongVectors = [[['Sufjan Stevens', 'The National'], ['Indie folk', 'Indie Rock'], 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]]
         
+        print "Calculating song difference vector"
         songDiffs = ru.getSongDifferences(testSongVectors)
         return len(songDiffs[0]) == len(testSongVectors[0])
 
@@ -38,11 +42,23 @@ def go():
         ru = REKTUser.User(debug=False)
         
         #call sub-tests
-        if testAddData(ru):
+        print "\nTesting ability to add song data... "
+        test1 = testAddData(ru)
+        print "Passed." if test1 else "Failed."
+        
+        print "\nTesting standard deviation calculation... "
+        test2 = testStdDev(ru)
+        print "Passed." if test2 else "Failed."
+        
+        print "\nTesting ability to create song difference vector... "
+        test3 = testSongDiffs(ru)
+        print "Passed." if test3 else "Failed."
+        
+        if test1:
             numPassed += 1
-        if testStdDev(ru):
+        if test2:
             numPassed += 1
-        if testSongDiffs(ru):
+        if test3:
             numPassed += 1
         
         #temp test
