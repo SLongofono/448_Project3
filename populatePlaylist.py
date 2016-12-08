398f72eb0d177c3ec0d8c4325f7268f7ac96330f
Stephen Longofono <onofognol@ku.edu>
2016-12-08 14:21:14 -0600

    Doxygen hooks and cleanup populatePlaylist.py

--- populatePlaylist.py
+++ populatePlaylist.py
@@ -1,3 +1,10 @@
+## @file populatePlaylist.py
+# @brief Gathers, vets and creates a playlist for songs
+# @details This script strings together the process of gathering songs to process,
+#	computing the weighted differnce from average, and adding the least different
+#	songs to a playlist on the user's Spotify account.  This script is called after
+#	updating the user's database in the run.py script
+#
 import spotipy
 import REKTUser
 import PlaylistGenerator
@@ -6,12 +13,27 @@ import Variance
 import config_obj
 from collections import OrderedDict
 
-# Minimum amount of new songs to add to playlist
+## @var newSongCount
+# @brief Minimum amount of new songs to add to playlist
 newSongCount = 100
 
-# Maximum weighted difference allowed for songs that make it into the list
+## @var threshold
+# @brief Maximum weighted difference allowed for songs that make it into the list
 threshold = 250
 
+
+## filterDifference
+# @brief Simple helper function to filter out dictionary entries
+#	with differences beyond a threshold value
+# @param songsDict A dictionary of the form Spotify track ID: weighted feature differences
+# @param threshold An optional parameter specifying the upper bound for the cum of the weighted
+#	feature differences
+# @return A list of track IDs associated with songs that had total difference less than the
+#	threshold
+# @details This is a helper method used to filter a dictionary type.  The input dictionary
+#	contains the weighted feature-wise difference of each track ID, the sum of which
+#	must be below the threshold for the song to be considered a match.
+#
 def filterDifference(songsDict, threshold=threshold):
 	results = []
 	for id, difference in songsDict.iteritems():
