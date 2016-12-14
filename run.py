689549225836de2853afc0935a50edca4e71ff92
Stephen Longofono <Onofognol@ku.edu>
2016-12-14 17:36:04 -0600

    Add Doxy hooks

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+## @file run.py
+# @brief Single entry point for our recommender system
+# @details This script runs Assemble_Profile.py to initiate or update the user profile database,
+#	and then calls populatePlaylist.py to generate a new playlist of songs.  It allows a single
+#	script to kick off the recommendation system.
+
 import os
 
 commands = ['python Assemble_Profile.py', 'python populatePlaylist.py']
